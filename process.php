2fb15faf986114b2bf3d9163085d474a76d14288
Sean Roddis <sean_roddis@hotmail.de>
2022-03-20 01:55:17 +0100

    changed mail

--- process.php
+++ process.php
@@ -3,7 +3,7 @@
 	$email = $_POST["email"];
 	$message = $_POST["message"];
 	 
-	$EmailTo = "youremail@gmail.com"; // change with your email
+	$EmailTo = "hello@roddis-consulting.com";
 	$Subject = "Portfolio CV/Resume";
 	 
 	// prepare email body text
